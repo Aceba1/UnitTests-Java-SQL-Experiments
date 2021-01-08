ed1b9e78dd94cf7ebf7977bc4245ef4674ca01f7
Arniel <aceba1@outlook.com>
2021-01-07 23:32:16 -0500

    Create SQLCreate.java

---
+++ src/main/java/com/aceba1/test/sqltest/utils/SQLCreate.java
@@ -0,0 +1,30 @@
+package com.aceba1.test.sqltest.utils;
+
+import com.aceba1.test.sqltest.model.ColumnProcessor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class SQLCreate {
+  // Make column types enum?
+  // Make column class
+  public static void createTable(
+    Connection connection,
+    String wrappedTableName,
+    String processorQuery
+  ) throws SQLException {
+    var statement = connection.createStatement();
+    statement.execute("CREATE TABLE " + wrappedTableName +
+      " AS " + processorQuery);
+    statement.close();
+  }
+
+  public static void createTable(
+    Connection connection,
+    String wrappedTableName
+  ) throws SQLException {
+    var statement = connection.createStatement();
+    statement.execute("CREATE TABLE " + wrappedTableName);
+    statement.close();
+  }
+}
